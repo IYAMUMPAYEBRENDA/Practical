9eab7cbd89ccdb4b1cd8447797f48dddfe6f2098
IYAMUMPAYEBRENDA <iyamumpayebrenda@gmail.com>
2025-09-26 15:03:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//VARIABLES
+
+let course_work_mark =26;
+let exam_mark =50;
+
+let final_mark = course_work_mark+exam_mark;
+console.log("The final score is :" + final_mark);
+console.log("when Divided it Equats to:" + (course_work_mark + exam_mark));
+
+
+const surname ="Brenda";
+const othername ="Mwiza";
+const fullname = surname +othername;
